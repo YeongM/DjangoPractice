cdc80ebbe16c15cc0f01d344b1faf8b2709ca64d
Kim YM <yeongm@hmail.com>
2021-11-19 14:32:04 +0900

    Question class and Answer class creat

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    subject = models.CharField(max_length=200)      #질문의 제목
+    content = models.TextField()                    #질문의 내용
+    create_data = models.DateTimeField()           #질문 시각
+    
+    
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)       #질문(어떤 질문의 답변인지 알기 위함)
+    content = models.TextField()                                           #질문 내용
+    create_date = models.DateTimeField()                                   #질문 시각
